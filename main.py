207d926aaca8724e43465834b3fa1974ae8a9fbb
Rommel <karlsrommel255@gmail.com>
2023-12-16 10:48:59 -0600

    Python0006

---
+++ main.py
@@ -0,0 +1,12 @@
+Hate = input("What do you hate")
+Like = input("What do you like")
+Family = input("how is the name of your family")  
+
+
+print("yeah i hate", Hate)
+print()
+print("I like", Like)
+print()
+print("pretty kind family i think", Family, "right?")
+print()
+      
